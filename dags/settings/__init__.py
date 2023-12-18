a946264e8ddd864b3ad1e4b76e6d0bccd2142449
Fabrizio Miano <fabriziomiano@gmail.com>
2023-12-18 11:50:56 +0100

    restructure

--- dags/settings/__init__.py
+++ dags/settings/__init__.py
@@ -1,9 +1,10 @@
 """
 Settings Module
 """
+from collections import OrderedDict
 import datetime as dt
 
-VERSION = "5.12"
+VERSION = "5.14"
 LOCKDOWN_DAY = dt.datetime(2020, 3, 22)
 PHASE2_DAY = dt.datetime(2020, 5, 4)
 PHASE3_DAY = dt.datetime(2020, 6, 15)
@@ -11,7 +12,48 @@ CRITICAL_AREAS_DAY = dt.datetime(2020, 11, 6)
 VACCINE_DAY = dt.datetime(2020, 12, 27)
 SERIES_DT_FMT = "d MMM yy"
 DOW_FMTY = 'EEE d MMM'
+KEY_PERIODS = OrderedDict()
 
+KEY_PERIODS["lockdown"] = {
+    "title": "Lockdown",
+    "text": "Lockdown",
+    "color": "red",
+    "from": LOCKDOWN_DAY,
+    "to": PHASE2_DAY,
+    "n_days": (PHASE2_DAY - LOCKDOWN_DAY).days,
+}
+KEY_PERIODS["phase2"] = {
+    "title": "Phase 2",
+    "text": '"Phase 2"',
+    "color": "orange",
+    "from": PHASE2_DAY,
+    "to": PHASE3_DAY,
+    "n_days": (PHASE3_DAY - PHASE2_DAY).days,
+}
+KEY_PERIODS["phase3"] = {
+    "title": "Phase 3",
+    "text": '"Phase 3"',
+    "color": "green",
+    "from": PHASE3_DAY,
+    "to": CRITICAL_AREAS_DAY,
+    "n_days": (CRITICAL_AREAS_DAY - PHASE3_DAY).days,
+}
+KEY_PERIODS["critical_areas"] = {
+    "title": "Critical Areas",
+    "text": "Critical areas",
+    "color": "red",
+    "from": CRITICAL_AREAS_DAY,
+    "to": dt.datetime.today(),
+    "n_days": (dt.datetime.today() - CRITICAL_AREAS_DAY).days,
+}
+KEY_PERIODS["vaccine_day"] = {
+    "title": "Vaccine day",
+    "text": "Vaccine day",
+    "color": "blue",
+    "from": VACCINE_DAY,
+    "to": dt.datetime.today(),
+    "n_days": (dt.datetime.today() - VACCINE_DAY).days,
+}
 ITALY_MAP = {
     'Abruzzo': ['Chieti', "L'Aquila", 'Pescara', 'Teramo'],
     'Basilicata': ['Matera', 'Potenza'],
