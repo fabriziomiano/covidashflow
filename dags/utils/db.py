dec50dd49749eaeef18df27c3a69e409e04c5de3
Fabrizio Miano <fabriziomiano@gmail.com>
2023-12-21 19:13:08 +0100

    first working version

--- dags/utils/db.py
+++ dags/utils/db.py
@@ -1,8 +1,5 @@
 """Common Mongo utils"""
 
-import os
-
 from airflow.providers.mongo.hooks.mongo import MongoHook
 
-conn_id = os.environ["AIRFLOW_CONN_MONGO_DEFAULT"]
-mongo_hook = MongoHook()
+mongo_hook = MongoHook("MONGO_DEFAULT")
