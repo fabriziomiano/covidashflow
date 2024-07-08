581b7daa1667229d30f100c089716e7554ca8a9a
Fabrizio Miano <fabriziomiano@gmail.com>
2024-07-09 00:26:56 +0200

    :hammer_and_wrench: add batch loading (#2)

--- dags/etl/dpc/load.py
+++ dags/etl/dpc/load.py
@@ -3,8 +3,39 @@ from utils.common import get_logger
 logger = get_logger("DPC-load")
 
 
-def load_dpc_records_to_mongo(records, collection, data_type, ordered=False):
-    logger.info(f"Loading {data_type} to {collection}")
+def load_dpc_records_to_mongo(
+        records,
+        collection,
+        data_type,
+        ordered=False,
+        batch_size=1000
+):
+    """
+    Loads records into a MongoDB collection in batches.
+
+    This function inserts records from a provided list into the specified MongoDB collection.
+    It processes the data in batches to manage memory usage and improve performance.
+    The collection is dropped before loading new data.
+
+    Parameters:
+    data (list): A list of dictionaries representing the records to be inserted into MongoDB.
+    collection (pymongo.collection.Collection): The MongoDB collection where the records will be inserted.
+    data_type (str): A string indicating the type of data being loaded, used for logging purposes.
+    ordered (bool, optional): If True, MongoDB will stop inserting at the first error. If False, MongoDB will continue with other inserts when an error occurs. Default is False.
+    batch_size (int, optional): The number of records to be inserted in each batch. Default is 1000.
+
+    Returns:
+    None
+
+    Logs the progress and results of the batch insertion, including the number of records inserted in each batch.
+    """
+
+    logger.info(f"Batch loading {len(records)} records of type {data_type} to {collection}")
     collection.drop()
-    result_many = collection.insert_many(records, ordered=ordered)
-    logger.info(f"Landed {len(result_many.inserted_ids)} records")
+    for i in range(0, len(records), batch_size):
+        batch = records[i:i + batch_size]
+        try:
+            collection.insert_many(batch, ordered=ordered)
+            logger.info(f"Inserted batch {i // batch_size + 1}")
+        except Exception as e:
+            print(f"Unexpected error inserting batch {i // batch_size + 1}: {e}")
